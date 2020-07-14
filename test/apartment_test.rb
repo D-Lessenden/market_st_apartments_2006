7ff99e76c355a1dc978f7ee2b5310d24c514d690
Daniel Lessenden <dless27@gmail.com>
2020-07-14 10:16:43 -0500

    Add test file and tests for apartment

---
+++ test/apartment_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/apartment'
+require 'pry'
+
+class ApartmentTest < Minitest::Test
+
+  def setup
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  end
+
+  def test_it_has_a_number
+    assert_equal "A1", @unti1.number
+  end
+
+  def test_monthly_rent
+    assert_equal 1200, @unit1.monthly_rent
+  end
+
+  def test_number_of_bathrooms
+    assert_equal 1, @unit1.bathrooms
+  end
+
+  def test_number_of_bedrooms
+    assert_equal 1, @unit1.bedrooms
+  end
+
+end
