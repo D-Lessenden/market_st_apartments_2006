c080034edb189151ff4b4044137ab0c0d184bc1a
Daniel Lessenden <dless27@gmail.com>
2020-07-14 10:38:22 -0500

    Create building test

---
+++ test/building_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/apartment'
+require './lib/renter'
+require './lib/building'
+require 'pry'
+
+class BuildingTest < Minitest::Test
+
+  def setup
+    @building = Building.new
+  end
+
+  def test_it_exists
+    assert_instance_of Building, @building 
+  end
+
+  def test_it_has_units
+    assert_equal [], @building.units
+  end
+
+end
