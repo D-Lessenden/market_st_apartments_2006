328fa1893a6fabdfa4b442b4cb3af1a21a64af48
Daniel Lessenden <dless27@gmail.com>
2020-07-14 10:30:29 -0500

    Add add_renter method

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,11 +1,16 @@
 class Apartment
-attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
 
   def initialize(apartment_hash)
     @number = apartment_hash[:number]
     @monthly_rent = apartment_hash[:monthly_rent]
     @bathrooms = apartment_hash[:bathrooms]
     @bedrooms = apartment_hash[:bedrooms]
+    @renter = []
+  end
+
+  def add_renter(name)
+    @renter << name
   end
 
 end
